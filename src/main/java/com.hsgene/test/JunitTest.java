f202ede1360496e5cd5aef25c022c0433896bec2
hanjunchen <812878994@qq.com>
2016-10-15 16:03:08 +0800

    SqlSessionTemplate

---
+++ src/main/java/com.hsgene/test/JunitTest.java
@@ -0,0 +1,36 @@
+package com.hsgene.test;
+
+import com.hsgene.entity.Employee;
+import org.junit.Before;
+import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by hjc on 2016/10/15.
+ */
+public class JunitTest {
+
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    protected ApplicationContext applicationContext;
+
+    @Before
+    public void setup(){
+        //  获取spring容器上下文
+        applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
+        //  SqlSessionTemplate存在于spring中，并且spring中的SqlSessionTemplate被注入了需要的属性，
+        //  所以只有从spring中拿到的SqlSessionTemplate才是我们需要的
+        //  每次访问数据库都会实例化一个SqlSessionTemplate
+        sqlSessionTemplate = applicationContext.getBean(SqlSessionTemplate.class);
+    }
+
+    @Test
+    public void testFindList(){
+        List<Employee> list = sqlSessionTemplate.selectList("com.hsgene.dao.EmployeeDao.findList");
+        list.forEach(x-> System.out.println(x.getName()));
+    }
+}
