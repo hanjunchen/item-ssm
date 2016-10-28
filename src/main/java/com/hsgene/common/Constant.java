9f397144ca04e29ac77270b51942b9a33c9c8d87
韩俊辰 <812878994@qq.com>
2016-10-28 15:03:16 +0800

    upload

--- src/main/java/com/hsgene/common/Constant.java
+++ src/main/java/com/hsgene/common/Constant.java
@@ -7,5 +7,5 @@ public class Constant {
 
     public static final String ACCESS_KEY = "KoXqNaWfEEQ6pXcBxFxH38XPUtS3cnQS0sRffSMy";
     public static final String SECRET_KEY = "zCk_yT-Y9_OjrDV-VRLkWrUUs_QwLJ5z0cdahw_r";
-    public static final String BUCKET_NAME = "hjc729";
+    public static final String BUCKET_NAME = "hjc729";  //  空间名
 }
--- src/main/java/com/hsgene/controller/UpTokenController.java
+++ src/main/java/com/hsgene/controller/UpTokenController.java
@@ -16,8 +16,8 @@ public class UpTokenController {
     //  使用七牛API生成上传凭证，凭证是动态的，所以每次上传都要获取一次
     @RequestMapping(value = "/uptoken", method = RequestMethod.GET)
     @ResponseBody
-    public String makeUpToken() {
+    public String makeUpToken(String key) {
         Auth auth = Auth.create(Constant.ACCESS_KEY, Constant.SECRET_KEY);
-        return auth.uploadToken(Constant.BUCKET_NAME, null, 3600l, null);
+        return auth.uploadToken(Constant.BUCKET_NAME, key, 600l, null);
     }
 }
