d2d88366044ca2c319c7f4ebf98829ea67c41aa9
韩俊辰 <812878994@qq.com>
2016-10-31 11:51:02 +0800

    xmlhttprequest和ajax

--- src/main/webapp/statics/myjs/dropzone.controller.js
+++ src/main/webapp/statics/myjs/dropzone.controller.js
@@ -11,8 +11,8 @@ function dropzoneCtrl($scope, $http, $filter) {
         var name = vm.dropzone.files[0].name;
         var ext = name.substring(name.lastIndexOf('.'), name.length);
         var timeStamp = $filter('date')(new Date(), 'MM-dd-HH-mm-ss');
-        vm.fileName = 'gene1' + timeStamp + ext;
-        var url = 'http://localhost:81/uptoken?key=' + vm.fileName;
+        vm.fileName = 'gene' + timeStamp + ext;
+        var url = 'http://localhost:8080/uptoken?key=' + vm.fileName;
         $http.get(url).then(function (res) {
             //vm.token = res.data;
             //vm.$apply();    //  异步请求相当于定时任务，需要手动digest才能更新到view和其他相连的作用域，这里直接用jq
