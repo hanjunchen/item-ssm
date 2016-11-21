8a838d7a64be513a4c1932bbb77464e1168fe09b
韩俊辰 <812878994@qq.com>
2016-11-21 16:02:17 +0800

    testDate

--- src/main/java/com/hsgene/controller/EmployeeController.java
+++ src/main/java/com/hsgene/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.hsgene.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -21,5 +22,12 @@ public class EmployeeController {
     public String findList(Employee employee, Model model) {
         model.addAttribute("list",employeeService.findList(employee));
         return "employeeList";
+    }
+
+    @RequestMapping(value = "/testDate")
+    @ResponseBody
+    public Employee testDate(){
+        //  日期字段默认被转为时间戳传到前台
+        return employeeService.findList(new Employee()).get(0);
     }
 }
