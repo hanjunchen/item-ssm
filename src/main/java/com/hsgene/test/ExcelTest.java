c144b4cc65c13c604cc85c3ed915c296edeecf9e
韩俊辰 <812878994@qq.com>
2016-12-06 10:37:54 +0800

    excel

---
+++ src/main/java/com/hsgene/test/ExcelTest.java
@@ -0,0 +1,40 @@
+package com.hsgene.test;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellUtil;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.junit.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by hjc on 2016/12/6.
+ */
+public class ExcelTest {
+
+    @Test
+    public void testExcel() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("E:/myExcel.xlsx");
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
+        Sheet sheet = workbook.createSheet("mySheet");
+        for (int i = 0; i < 10; i++) {
+            Row row = sheet.createRow(i);
+            for (int j = 0; j < 11; j++) {
+                if (i == 0) {
+                    // 首行
+                    row.createCell(j).setCellValue("column" + j);
+                } else {
+                    // 首列
+                    if (j == 0) {
+                        CellUtil.createCell(row, j, String.valueOf(i));
+                    } else
+                        CellUtil.createCell(row, j, String.valueOf(Math.random()));
+                }
+            }
+        }
+        workbook.write(fileOutputStream);
+        fileOutputStream.close();
+    }
+}
