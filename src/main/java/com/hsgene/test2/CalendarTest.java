03a5deaa3f13cff837f709b742d3c94d48cdcfb1
hanjunchen <812878994@qq.com>
2017-01-16 16:37:24 +0800

    定时任务

---
+++ src/main/java/com/hsgene/test2/CalendarTest.java
@@ -0,0 +1,26 @@
+package com.hsgene.test2;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by hjc on 2017/1/16.
+ */
+public class CalendarTest {
+
+    /**
+     * 获取当月的第一天的 00:00:00
+     */
+    @Test
+    public void testCalendar() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(dateFormat.format(calendar.getTime()));
+    }
+}
