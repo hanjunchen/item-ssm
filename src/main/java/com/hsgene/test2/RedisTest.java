40046caf85746960a50c32c5ae68153be84ee24c
hanjunchen <812878994@qq.com>
2017-01-17 15:31:01 +0800

    redis

---
+++ src/main/java/com/hsgene/test2/RedisTest.java
@@ -0,0 +1,9 @@
+package com.hsgene.test2;
+
+/**
+ * Created by hjc on 2017/1/17.
+ */
+public class RedisTest {
+
+
+}
---
+++ src/main/java/com/hsgene/util/RedisUtil.java
@@ -0,0 +1,8 @@
+package com.hsgene.util;
+
+/**
+ * Created by hjc on 2017/1/17.
+ */
+public class RedisUtil {
+
+}
