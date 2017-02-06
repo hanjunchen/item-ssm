152005ff55f6c411351151c3be446e073410e158
hanjunchen <812878994@qq.com>
2017-02-06 17:39:51 +0800

    pom

--- src/main/java/com/hsgene/test3/RedisTest.java
+++ src/main/java/com/hsgene/test3/RedisTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.data.redis.core.BoundListOperations;
 import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -35,11 +34,11 @@ public class RedisTest {
         BoundValueOperations valueOperations = redisTemplate.boundValueOps("memberIds");
         valueOperations.set(JSON.toJSONString(list));
         valueOperations.expire(1, TimeUnit.HOURS);
-        System.out.println(JSON.parseArray((String) valueOperations.get()).get(0));
-        List<String> list1 = Lists.newArrayList("abc","def","ghi");
+        System.out.println(valueOperations.get());
+        /*List<String> list1 = Lists.newArrayList("abc","def","ghi");
         System.out.println(list1.get(2));
         BoundListOperations listOperations = redisTemplate.boundListOps("memberList");
         listOperations.set(1L,JSON.toJSONString(list1));
-        System.out.println(listOperations.index(1L));
+        System.out.println(listOperations.index(1L));*/
     }
 }
